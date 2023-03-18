da9a54a8497d673d83a20cf402d26a7f5634b638
Kiet <n18dccn090@student.ptithcm.edu.vn>
2023-03-18 10:46:57 +0700

    layout

--- src/component/Layouts/DefaultLayout/Sidebar/index.js
+++ src/component/Layouts/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,13 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss";
+
+const cx = classNames.bind(styles);
 function Sidebar() {
-  return <h1>Sidebar</h1>;
+  return (
+    <div className={cx("wrapper", "col-4")}>
+      <h2>Side bar</h2>
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/component/Layouts/DefaultLayout/index.js
+++ src/component/Layouts/DefaultLayout/index.js
@@ -1,15 +1,21 @@
+import classNames from "classnames/bind";
+import styles from "./DefaultLayout.module.scss";
+import { Fragment } from "react";
 import Header from "../component/Header";
 import Sidebar from "./Sidebar";
 
+const cx = classNames.bind(styles);
 function Defaultlayout({ children }) {
   return (
-    <div className="wrapper">
+    <Fragment>
       <Header />
-      <div className="container">
-        <Sidebar />
-        <div className="content">{children}</div>
+      <div className={cx("wrapper")}>
+        <div className={cx("wrapper_container", "row")}>
+          <Sidebar />
+          <div className={cx("col-8")}>{children}</div>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
